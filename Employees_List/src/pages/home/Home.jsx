e37568224e5d30b1eb59053fd369b7025f06c74a
Americano.Dev <andrenations@gmail.com>
2024-01-31 19:24:13 -0500

    added images, useEffect for the data fetching process and the useState for loading

--- Employees_List/src/pages/home/Home.jsx
+++ Employees_List/src/pages/home/Home.jsx
@@ -1,48 +1,104 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
+import Loader from '../../components/loader/Loader';
+
+import './home.css';
+
+import img1 from '../../assets/images/alice.jpg';
+import img2 from '../../assets/images/bob.jpg';
+import img3 from '../../assets/images/chris.jpg';
+import img4 from '../../assets/images/emma.jpg';
+import img5 from '../../assets/images/john.jpg';
+import img6 from '../../assets/images/oliva.jpg';
+import img7 from '../../assets/images/jane.jpg';
+import img8 from '../../assets/images/ryan.jpg';
+import img9 from '../../assets/images/tayler.jpg';
+import img10 from '../../assets/images/black.jpg';
+
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/users');
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setData(result);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="d-flex flex-column justify-content-center align-items-center bg-light vh-100">
-      <div className="w-75 rounded bg-white border media shadow p-4">
-        <div className="table-responsive">
-          <table className="table table-stipend">
-            <thead>
-              <tr>
-                <th className="id">ID</th>
+      {isLoading && <Loader />}
+      {error && <p>Error: {error}</p>}
+      {!isLoading && !error && (
+        <div className="w-75 rounded bg-white border media shadow p-4">
+          <div className="table-responsive">
+            <table className="table table-stipend">
+              <thead>
+                <tr>
+                  <th className="id">ID</th>
+                  <th>Photo</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((item, index) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
 
-                <th>Name</th>
-                <th>Email</th>
-                <th>Phone</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              <td>1</td>
-              <td>name</td>
-              <td>email</td>
-              <td>phone</td>
-              <td>
-                <Link className="me-2">
-                  <i className="fa fa-pencil" aria-hidden="true">
-                    <FaPencilAlt />
-                    User Details
-                  </i>
-                </Link>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <img src={images[index]} className="rounded-circle me-2" style={{ width: '30px', height: '30px' }} />
+                      </div>
+                    </td>
 
-                <Link className="btn btn-sm btn-warning media-btn me-2">Edit</Link>
+                    <td>{item.name}</td>
+                    <td>{item.email}</td>
+                    <td>{item.phone}</td>
+                    <td>
+                      <Link className="me-2">
+                        <i className="fa fa-pencil" aria-hidden="true">
+                          <FaPencilAlt />
+                          User Details
+                        </i>
+                      </Link>
 
-                <Button className="btn btn-sm media-btn btn-danger">
-                  <i className="fa fa-eye" aria-hidden="true">
-                    <FaTrashAlt />
-                  </i>
-                </Button>
-              </td>
-            </tbody>
-          </table>
+                      <Link className="btn btn-sm btn-warning media-btn me-2">Edit</Link>
+
+                      <Button className="btn btn-sm media-btn btn-danger">
+                        <i className="fa fa-eye" aria-hidden="true">
+                          <FaTrashAlt />
+                        </i>
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
